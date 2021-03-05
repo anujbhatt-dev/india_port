f64caee83c818a3d35501e7ee99e366416e8aaa3
anuj bhatt <anujbhatt023@gmail.com>
2021-03-05 17:57:55 +0530

    landing 4 designed

--- src/components/layout/landing/landing4/landing4.jsx
+++ src/components/layout/landing/landing4/landing4.jsx
@@ -1,4 +1,8 @@
 import React, {Component} from "react"
+import bigCircle from "../../../../assets/images/bigCircle.svg"
+import smallTruck from "../../../../assets/images/smallTruck.svg"
+import smallShip from "../../../../assets/images/smallShip.svg"
+import yellowPlane from "../../../../assets/images/yellowPlane.svg"
 
 class Landing4 extends Component{
 
@@ -6,7 +10,14 @@ class Landing4 extends Component{
 
       return (
          <div className="landing4">
-             landing4
+             <div className="landing4__text">
+                <div className="landing4__text_primary">indiaport is a goal is to improve</div>
+                <div className="landing4__text_secondary">transparency</div>
+             </div>
+             <img className="landing4__circle" src={bigCircle} alt=""/>
+             <img className="landing4__smallShip" src={smallShip} alt=""/>
+             <img className="landing4__smallTruck" src={smallTruck} alt=""/>
+             <img className="landing4__yellowPlane" src={yellowPlane} alt=""/>
          </div>
       )
     }
--- src/components/layout/landing/landing7/landing7.jsx
+++ src/components/layout/landing/landing7/landing7.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react"
 import b1 from "../../../../assets/images/b1.svg"
+import {NavLink} from "react-router-dom"
 
 class Landing7 extends Component{
 
@@ -44,6 +45,13 @@ class Landing7 extends Component{
                     <div className="landing7__items_card-title">Lorem ipsum dolor sit.</div>
                 </div>
 
+             </div>
+
+             <div className="landing7__btn">
+                 <span>more</span>
+                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="38" viewBox="0 0 18 38">
+                    <path id="Polygon_7" data-name="Polygon 7" d="M16.937,1.955a3,3,0,0,1,4.126,0L32.534,12.822A3,3,0,0,1,30.471,18H7.529a3,3,0,0,1-2.063-5.178Z" transform="translate(18) rotate(90)" fill="#0239ff"/>
+                 </svg>
              </div>
          </div>
       )
