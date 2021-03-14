f2b899be02bf2302b2edfd0ead4f36e3601e4a05
sagarmachine <sagarmonkey800@gmail.com>
2021-03-14 16:57:03 +0530

    cbm calculator algo implemented

--- src/components/layout/cbmCalc/cbmCalc.jsx
+++ src/components/layout/cbmCalc/cbmCalc.jsx
@@ -14,7 +14,97 @@ import add from "../../../assets/images/add.svg"
 import Aos from "aos"
 import "aos/dist/aos.css"
 
+
 class CbmCalc extends Component{
+
+   state={
+
+      level:0,
+      // invalids:{1:1,2:1,3:1,4:1,5:1},
+      valids:{},
+      data:{},
+      updated:true,
+      total:null,
+
+  }
+
+
+  componentDidUpdate=()=>{
+
+      // console.log(this.state)
+
+      if(!this.state.updated){
+          let data={...this.state.data};
+
+          Object.keys(this.state.valids).forEach(key=>{
+              // alert(key)
+              data[key]={...data[key], ans:((data[key].len*data[key].wid*data[key].hei))/(data[key].uni==="cm"?1000000:1)}
+              
+          })
+
+          this.setState({updated:true, data:data});
+
+      }
+
+  }
+
+  addLevel=()=>{
+      this.setState(s=>{return {level:s.level+1}})
+  }
+
+  validToogle=(i, valid)=>{
+         let valids={...this.state.valids};
+         let invalids={...this.state.invalids};
+
+         if(!valid){
+             invalids={...invalids,[i]:1};
+             delete valids[i];
+         }else{
+             valids={...valids,[i]:1};
+             delete invalids[i];
+         }
+
+         this.setState({valids:valids, invalids:invalids, updated:false})
+  }
+
+  onChange=(id,e)=>{
+      
+      // alert(e.target.value)
+      let val=e.target.value;
+      let name=e.target.name;
+      let data={...this.state.data};
+
+      if(val===""){
+          delete data[id][name];
+          this.validToogle(id, false);
+          
+      }else{
+        
+          data[id]={...data[id], [name]:val};
+          if((data[id].len&&data[id].wid&&data[id].hei)||(data[id].len&&data[id].wid&&name==="hei")||(data[id].hei&&data[id].wid&&name==="len")||(data[id].hei&&data[id].len&&name==="wid"))
+            {
+                this.validToogle(id, true);
+                
+           }
+         }
+
+         this.setState({data:data, updated:false, total:null});
+  }
+
+  calculateTotal=()=>{
+
+      let total=0;
+
+      Object.keys(this.state.valids).forEach(key=>{
+        
+          total+=this.state.data[key].ans;
+
+      })
+
+      this.setState({total:total});
+
+  }
+
 
 
 
@@ -47,96 +137,89 @@ class CbmCalc extends Component{
 
       return (
          <div className="cbmCalc ">
-             {!this.props.mobile?<div className="invoice__box">
-                 <img className="invoice__box_bot" src={leaf2} alt=""/>
-                 <img data-aos="fade-up" className="invoice__box_big cbmCalc__box_big" src={box} alt=""/>
-                 <img className="invoice__box_i1" src={i} alt=""/>
-                 <img className="invoice__box_arr" src={halfArrow} alt=""/>
-                 <div className="cbmCalc__head">CBM Calculator</div>
-                 <div className="invoice__box_go">GO</div>
-                 <div className="cbmCalc__box_total  cbmCalc__mob--total">
-                    <span className="cbmCalc__box_total-text">Total</span> <span className="cbmCalc__box_total-number">123456</span>
-                 </div>
+             {!this.props.mobile?
+                <div className="invoice__box">
+                <img className="invoice__box_bot" onClick={this.calculateTotal} src={leaf2} alt=""/>
+                <img data-aos="fade-up" className="invoice__box_big cbmCalc__box_big" src={box} alt=""/>
+                <img className="invoice__box_i1" src={i} alt=""/>
+                <img className="invoice__box_arr" src={halfArrow} alt=""/>
+                <div className="cbmCalc__head">CBM Calculator</div>
+                <div className="invoice__box_go" onClick={this.calculateTotal}>GO</div>
+                <div className="cbmCalc__box_total  cbmCalc__mob--total">
+                <span className="cbmCalc__box_total-text">Total</span>
+                   {this.state.total?
+                    <span className="cbmCalc__box_total-number">{this.state.total}</span>:
+                    <span className="cbmCalc__box_total-number">-</span>}
+                </div>
+    
+                <table cellspacing="15" className="cbmCalc__table">
+                   <thead>
+                      <td>Sr.No.</td>
+                      <td>Length</td>
+                      <td>Width</td>
+                      <td>Height</td>
+                      <td>Unit</td>
+                      <td>CBM</td>
+                   </thead>
+                   <tbody>
+                      {[1,2,3,4,5].map(id=>
+                      <tr>
+                         <td className="cbmBack__table-sr"><p className="cbmCalc__p">{this.state.valids[id]?id:"invalid"}</p></td>
+                         <td  className="cbmBack__table-input"><input onChange={(e)=>this.onChange(id,e)}  value={this.state.data[id]&&this.state.data[id].len?this.state.data[id].len:null} placeholder="len" name="len" type="text"/></td>
+                         <td  className="cbmBack__table-input"><input onChange={(e)=>this.onChange(id,e)}  value={this.state.data[id]&&this.state.data[id].wid?this.state.data[id].wid:null} placeholder="wid" name="wid" type="text"/></td>
+                         <td  className="cbmBack__table-input"><input onChange={(e)=>this.onChange(id,e)}  value={this.state.data[id]&&this.state.data[id].hei?this.state.data[id].hei:null} placeholder="hei" name="hei" type="text"/></td>
+                        
+                         <td className="cbmBack__table-input">
+                            <select onChange={(e)=>this.onChange(id,e)} name="uni"  id="">
+                               <option value="m">m</option>
+                               <option value="cm">cm</option>
+                            </select>
+                            </td>
+                         <td className=" cbmCalc__box_total-number">{this.state.valids[id]?this.state.data[id].ans:"-"}</td>   
+                        </tr>
+                      )}
+                   </tbody>
+                </table>
+            </div>:
+            [ <h1 className="cbmCalc__mob_head">cbm calculator</h1>,
+            <div className="cbmCalc__mob">
+               <img className="invoice__box_bot cbmCalc__mob--bot" onClick={this.calculateTotal}  src={leaf2} alt=""/>
+               <div className="cbmCalc__box_total  cbmCalc__mob--total">
+               <span className="cbmCalc__box_total-text">Total</span>
+               {this.state.total?
+                <span className="cbmCalc__box_total-number">{this.state.total}</span>:
+                <span className="cbmCalc__box_total-number">-</span>}
+               </div>
+               <div className="cbmCalc__mob_flex">
+                 {/* <span  className="cbmCalc__mob_flex-span0 cbmCalc__mob_flex-span1"></span> */}
+                 <span placeholder="len" className="cbmCalc__mob_flex-span2" type="text">Length</span>
+                 <span placeholder="len" className="cbmCalc__mob_flex-span2" type="text">Width</span>
+                 <span placeholder="len" className="cbmCalc__mob_flex-span2" type="text">Height</span>
+                 <span placeholder="len" className="cbmCalc__mob_flex-span2" type="text">Unit</span>
+                 <span placeholder="len" className="cbmCalc__mob_flex-span2" type="text">CBM</span>
+               </div>
 
-                 <table cellspacing="15" className="cbmCalc__table">
-                    <thead>
-                       <td>Sr.No.</td>
-                       <td>Length</td>
-                       <td>Width</td>
-                       <td>Height</td>
-                       <td>Unit</td>
-                       <td>Unit</td>
-                    </thead>
-                    <tbody>
-                       <tr>
-                          <td className="cbmBack__table-sr"><p className="cbmCalc__p">1</p></td>
-                          <td className="cbmBack__table-input"><input placeholder="len" type="text"/></td>
-                          <td className="cbmBack__table-input"><input placeholder="wid" type="text"/></td>
-                          <td className="cbmBack__table-input"><input placeholder="height" type="text"/></td>
-                          <td className="cbmBack__table-input"><input placeholder="unit" type="text"/></td>
-                          <td className="cbmCalc__box_total-number"><span>123</span></td>
-                       </tr>
-
-                       <tr>
-                          <td className="cbmBack__table-sr"><p className="cbmCalc__secP cbmCalc__p"><img src={plus} alt="plus"/></p></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmCalc__box_total-number cbmCalc__box_total-number--background"><img src={dash} alt=""/></td>
-                       </tr>
-
-                       <tr>
-                          <td className="cbmBack__table-sr"><p className="cbmCalc__secP cbmCalc__p"><img src={plus} alt="plus"/></p></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmCalc__box_total-number cbmCalc__box_total-number--background"><img src={dash} alt=""/></td>
-                       </tr>
-
-                       <tr>
-                          <td className="cbmBack__table-sr"><p className="cbmCalc__secP cbmCalc__p"><img src={plus} alt="plus"/></p></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmCalc__box_total-number cbmCalc__box_total-number--background"><img src={dash} alt=""/></td>
-                       </tr>
-
-                       <tr>
-                          <td className="cbmBack__table-sr"><p className="cbmCalc__secP cbmCalc__p"><img src={plus} alt="plus"/></p></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmBack__table-input"><input disabled={true} placeholder="" type="text"/></td>
-                          <td className="cbmCalc__box_total-number cbmCalc__box_total-number--background"><img src={dash} alt=""/></td>
-                       </tr>
-                    </tbody>
-                 </table>
-             </div>:
-             [ <h1 className="cbmCalc__mob_head">cbm calculator</h1>,
-               <div className="cbmCalc__mob">
-                  <img className="invoice__box_bot cbmCalc__mob--bot" src={leaf2} alt=""/>
-                  <div className="cbmCalc__box_total  cbmCalc__mob--total">
-                     <span className="cbmCalc__box_total-text">Total</span> <span className="cbmCalc__box_total-number">123456</span>
-                  </div>
+               {[1,2,3,4,5].map(id=>
                   <div className="cbmCalc__mob_flex">
-                    <span  className="cbmCalc__mob_flex-span0 cbmCalc__mob_flex-span1">1.</span>
-                    <input placeholder="len" type="text"/>
-                    <input placeholder="len" type="text"/>
-                    <input placeholder="len" type="text"/>
-                    <input placeholder="len" type="text"/>
-                  </div>
-                  <div  className="cbmCalc__mob_flex">
-                     <span className="cbmCalc__mob_flex-span0 cbmCalc__mob_flex-span2">{""}</span>
-                     <span className="cbmCalc__mob_flex-span2">Width</span>
-                     <span className="cbmCalc__mob_flex-span2">Length</span>
-                     <span className="cbmCalc__mob_flex-span2">Height</span>
-                     <span className="cbmCalc__mob_flex-span2">Unit</span>
-                  </div>
-             </div>,
-             <img src={add} alt=""/>]
+                      {/* <span  className="cbmCalc__mob_flex-span0 cbmCalc__mob_flex-span1">{id}</span> */}
+                    <input  onChange={(e)=>this.onChange(id,e)}  value={this.state.data[id]&&this.state.data[id].len?this.state.data[id].len:null} placeholder="len" name="len" type="text"/>
+                    <input  onChange={(e)=>this.onChange(id,e)}  value={this.state.data[id]&&this.state.data[id].wid?this.state.data[id].wid:null} placeholder="wid" name="wid" type="text"/>
+                    <input  onChange={(e)=>this.onChange(id,e)}  value={this.state.data[id]&&this.state.data[id].hei?this.state.data[id].hei:null} placeholder="hei" name="hei" type="text"/>
+                    <span className="cbmCalc__mob_flex-span2">
+                        <select onChange={(e)=>this.onChange(id,e)} name="uni"  id="">
+                           <option value="m">m</option>
+                           <option value="cm">cm</option>
+                        </select>
+                        
+                        </span> 
+                     <span className=" cbmCalc__box_total-number">{this.state.valids[id]?this.state.data[id].ans:"-"}</span>   
+                    </div>
+                  )}
+               <div  className="cbmCalc__mob_flex">
+                  
+               </div>
+          </div>,
+         ]
            }
 
 
