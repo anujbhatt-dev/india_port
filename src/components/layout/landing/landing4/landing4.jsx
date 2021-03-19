003316442d3ce17a0a753e97fa2b6f19daaca0c7
anuj bhatt <anujbhatt023@gmail.com>
2021-03-19 15:46:52 +0530

    landing4 correction

--- src/components/layout/landing/landing4/landing4.jsx
+++ src/components/layout/landing/landing4/landing4.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react"
 import triangle from "../../../../assets/images/triangle.svg"
+import circle from "../../../../assets/images/bigCircle.svg"
 import smallTruck from "../../../../assets/images/smallTruck.svg"
 import smallShip from "../../../../assets/images/smallShip.svg"
 import yellowPlane from "../../../../assets/images/yellowPlane.svg"
@@ -8,16 +9,16 @@ class Landing4 extends Component{
 
     render(){
 
-      // <img className="landing4__smallShip" src={smallShip} alt=""/>
-      // <img className="landing4__smallTruck" src={smallTruck} alt=""/>
-      // <img className="landing4__yellowPlane" src={yellowPlane} alt=""/>
       return (
          <div  id="landing4" className="landing4">
              <div className="landing4__text">
-                <div className="landing4__text_primary">indiaport is a goal is to improve</div>
-                <div className="landing4__text_secondary">transparency</div>
+               <div className="landing4__text_primary">indiaport is a goal is to improve</div>
+               <div className="landing4__text_secondary">transparency</div>
              </div>
-             <img className="landing4__circle" src={triangle} alt=""/>
+                <img className="landing4__circle" src={circle} alt=""/>
+                <img className="landing4__smallShip" src={smallShip} alt=""/>
+                <img className="landing4__smallTruck" src={smallTruck} alt=""/>
+                <img className="landing4__yellowPlane" src={yellowPlane} alt=""/>
          </div>
       )
     }
