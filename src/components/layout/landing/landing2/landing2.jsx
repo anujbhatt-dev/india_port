b5d5015741930ffecad517857023f0c2a3d7a92c
anuj bhatt <anujbhatt023@gmail.com>
2021-03-19 14:08:48 +0530

    landing2 animation done

--- src/components/layout/landing/landing2/landing2.jsx
+++ src/components/layout/landing/landing2/landing2.jsx
@@ -1,167 +1,113 @@
 import React, {Component} from "react"
-import $ from 'jquery'
 import mob1 from "../../../../assets/images/mob1.svg"
 import mob2 from "../../../../assets/images/mob2.svg"
-import boat1 from "../../../../assets/images/boat1.svg"
-import boat2 from "../../../../assets/images/boat2.svg"
-import boat3 from "../../../../assets/images/boat3.svg"
+import smallTruck from "../../../../assets/images/smallTruck.svg"
+import line from "../../../../assets/images/line.svg"
+import bar from "../../../../assets/images/bar.svg"
+import smallShip from "../../../../assets/images/smallShip.svg"
+import gsap from "gsap"
 import ScrollMagic from 'scrollmagic';
-import {TimelineMax, Power3 , TweenMax} from "gsap/gsap-core"
-// import Timeline from 'timeline';
-
+// import {TimelineMax, Power3 , TweenMax} from "gsap";
+// import {TimelineMax} from "gsap/gsap-core"
+import {TimelineMax} from "gsap/gsap-core"
+import {Power3} from "gsap/gsap-core"
+import {TweenMax} from "gsap/gsap-core"
 import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+
 ScrollMagicPluginGsap(ScrollMagic, TweenMax, TimelineMax);
+
 
 class Landing2 extends Component{
 
-    state={
-      number:0,
-      mobileNumber:0,
-      flag:0
-    }
-
     componentDidMount=()=>{
-        // window.addEventListener("scroll",(e)=>{
-        //    console.log(window.pageYOffset);
-        //    if((window.pageYOffset>=(this.props.landing2-100)) && (window.pageYOffset<=this.props.landing2 &&(this.state.flag===0)) && !this.props.mobile){
-        //      $(".landing2").css("position","fixed")
-        //      $("body").css("overflow-y","hidden")
-        //      setTimeout(()=>{
-        //         this.setState({
-        //           number:1
-        //         })
-        //         setTimeout(()=>{
-        //            this.setState({
-        //              number:2
-        //            })
-        //            setTimeout(()=>{
-        //               this.setState({
-        //                 number:0,flag:1
-        //               })
-        //               $(".landing2").css("position","relative")
-        //               $("body").css("overflow-y","scroll")
-        //            },2000)
-        //         },2000)
-        //      },2000)
-        //    }
-        // })
 
-        let controller = new ScrollMagic.Controller();
 
-        let timeline = new TimelineMax();
-        timeline
-        .from(
-          ".landing2__flex_text",
-          3,
-          {
-            y:-100,
-            ease:Power3.easeInOut
-          }
-        )
+      let controller = new ScrollMagic.Controller();
+       let timeline = new TimelineMax();
 
-        let scene = new ScrollMagic.Scene({
-          triggerElement:".landing2",
-          duration:"100%",
-          triggerHook:0,
-          offset:"300"
+      timeline
+      .to('.landing2__firstWrapper_first-text--3', 20, {
+        y: "100%",
+        x: 0,
+        ease: Power3.linear
+       })
+       .to('.landing2__secondWrapper_second--1', 20, {
+         y: "-100%",
+         x:0,
+         ease: Power3.linear
+       },"-=20")
+       .to('.landing2__firstWrapper_first-text--2', 20, {
+         y: "100%",
+         x:0,
+         ease: Power3.easeInOut
         })
-        .setTween(timeline)
-        .setPin(".landing2")
-        .addTo(controller)
+        .to('.landing2__secondWrapper_second--2', 20, {
+          y: "-100%",
+          x:0,
+          ease: Power3.linear
+        },"-=20")
+        .to('.landing2__motion_bar', 50, {
+          x: 900,
+          ease: Power3.linear
+        },"-=50")
+        .to('.landing2__motion_mode', 50, {
+          x: 900,
+          ease: Power3.linear
+        },"-=50")
+
+
+       let scene = new ScrollMagic.Scene({
+         triggerElement: '#landing2',
+         duration: '100%',
+         triggerHook: 0,
+         offset: '0'
+     })
+     .setTween(timeline)
+     .setPin('#landing2')
+     .addTo(controller);
 
 
 
 
 
-        this.numberInterval();
-
-
-
-    }
-
-
-    mobileNumberIntervalFunc=null;
-
-    numberInterval=()=>{
-     this.numberIntervalFunc= setInterval(()=>{
-          this.setState(s=>{return {mobileNumber:s.mobileNumber===2?0:s.mobileNumber+1}})
-      }, 1000)
-    }
-
-    mobileNumberHandler=(mobileNumber)=>{
-      this.setState({
-        mobileNumber:mobileNumber
-      })
-      clearInterval(this.mobileNumberIntervalFunc);
-    }
-
-    numberHandler=(number)=>{
-      this.setState({
-        number:number
-      })
     }
 
     render(){
 
       return (
-         !this.props.mobile?<div className="landing2">
-         <div className="landing2__dots">
-
-
-             <div onClick={()=>this.numberHandler(0)} className={this.state.number===0?"landing2__dots_div landing2__dots_div--mod":"landing2__dots_div"}>
-                  .
-             </div>
-
-             <div onClick={()=>this.numberHandler(1)} className={this.state.number===1?"landing2__dots_div landing2__dots_div--mod":"landing2__dots_div"}>
-                  .
-             </div>
-
-             <div onClick={()=>this.numberHandler(2)} className={this.state.number===2?"landing2__dots_div landing2__dots_div--mod":"landing2__dots_div"}>
-                  .
-             </div>
-
-         </div>
-            <div  id="landing2" className="landing2__flexWrapper">
-
-               <div className="landing2__flex">
-                   <div className="landing2__flex_text">
-                       <div className="landing2__flex_text-l">{this.state.number===0?"Duty":this.state.number===1?"Invoice":"BGM"}<br/>Calculator</div>
-                       <div className="landing2__flex_text-s">Lorem ipsum dolor sit amet,<br/>consetetur sadipscing elitr, sed diam nonumy eirmod</div>
-                   </div>
-                   <img src={mob1} alt=""/>
-               </div>
-               <img src={this.state.number===0?boat1:this.state.number===1?boat2:boat3} alt=""/>
-             </div>
-
-         </div>
-         :
-         <div className="landing2__mob">
-               <div className="landing2__mob_head">Tools</div>
-              <div className="landing2__mob_dots">
-
-                  <svg onClick={()=>this.mobileNumberHandler(0)} xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15">
-                    <circle id="Ellipse_259" data-name="Ellipse 259" cx="7.5" cy="7.5" r="7.5" stroke="#0239ff" fill={this.state.mobileNumber===0?"#0239ff":"white"}/>
-                  </svg>
-
-                  <svg onClick={()=>this.mobileNumberHandler(1)} xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15">
-                    <circle id="Ellipse_259" data-name="Ellipse 259" cx="7.5" cy="7.5" r="7.5" stroke="#0239ff" fill={this.state.mobileNumber===1?"#0239ff":"white"}/>
-                  </svg>
-
-                  <svg onClick={()=>this.mobileNumberHandler(2)} xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 15 15">
-                    <circle id="Ellipse_259" data-name="Ellipse 259" cx="7.5" cy="7.5" r="7.5" stroke="#0239ff" fill={this.state.mobileNumber===2?"#0239ff":"white"}/>
-                  </svg>
-
-              </div>
-              <div className="landing2__mob_main">
-                   <img  src={this.state.mobileNumber===0?mob1:this.state.mobileNumber===1?mob2:mob1} alt=""/>
-                   <div className="landing2__mob_main-name">
-                       {this.state.mobileNumber===0?"mob1":this.state.mobileNumber===1?"mob2":"mob1"} Calculator
-                   </div>
-              </div>
-         </div>
-
+                <div id="landing2" className="landing2">
+                    <div className="landing2__firstWrapper">
+                        <div className="landing2__firstWrapper_first">
+                             <div className="landing2__firstWrapper_first-text landing2__firstWrapper_first-text--1">
+                                 <div className="landing2__firstWrapper_first-text-head">Duty Calculator</div>
+                                 <div className="landing2__firstWrapper_first-text-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, magnam.</div>
+                             </div>
+                             <div className="landing2__firstWrapper_first-text  landing2__firstWrapper_first-text--2 ">
+                                 <div className="landing2__firstWrapper_first-text-head">Invoice Calculator</div>
+                                 <div className="landing2__firstWrapper_first-text-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, magnam.</div>
+                             </div>
+                             <div className="landing2__firstWrapper_first-text  landing2__firstWrapper_first-text--3  ">
+                                 <div className="landing2__firstWrapper_first-text-head">BGM Calculator</div>
+                                 <div className="landing2__firstWrapper_first-text-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, magnam.</div>
+                             </div>
+                        </div>
+                    </div>
+                    <div className="landing2__secondWrapper">
+                        <div className="landing2__secondWrapper_second">
+                          <img className="landing2__secondWrapper_second--1" src={mob1} alt=""/>
+                          <img className="landing2__secondWrapper_second--2" src={mob2} alt=""/>
+                          <img className="landing2__secondWrapper_second--3" src={mob1} alt=""/>
+                        </div>
+                    </div>
+                    <div className="landing2__motion">
+                        <img  className="landing2__motion_mode" src={smallShip} alt=""/>
+                        <img  className="landing2__motion_bar" src={bar} alt=""/>
+                        <img  className="landing2__motion_line" src={line} alt=""/>
+                    </div>
+                </div>
       )
     }
 }
 
+// <img className="landing2__truck" src={smallTruck} alt=""/>
 export default Landing2;
