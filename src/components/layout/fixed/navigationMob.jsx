053c5dd02256ba8e2a8f3ce4beeadefd2bf440e4
anuj bhatt <anujbhatt023@gmail.com>
2021-05-23 23:21:30 +0530

    fixes

--- src/components/layout/fixed/navigationMob.jsx
+++ src/components/layout/fixed/navigationMob.jsx
@@ -26,16 +26,16 @@ import {NavLink} from "react-router-dom"
                 </div>
 
                 <div className={this.state.open?"openNav navMob__items ":"closeNav navMob__items "}>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/" className="navigation__items_item navigation__items_item">Home</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/diary" className="navigation__items_item navigation__items_item">Diary</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/CbmCalc" className="navigation__items_item navigation__items_item">CBM</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/invoice" className="navigation__items_item navigation__items_item">Invoice</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/about" className="navigation__items_item navigation__items_item">About</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/contact" className="navigation__items_item navigation__items_item">Contact</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/exim" className="navigation__items_item navigation__items_item">Exim</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/freight" className="navigation__items_item navigation__items_item">Freight</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/insurance" className="navigation__items_item navigation__items_item">Insurance</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{color:"white",background:"#FFBF29"}} exact to="/customClearance" className="navigation__items_item navigation__items_item">Custom</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/" className="navigation__items_item navigation__items_item">Home</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/diary" className="navigation__items_item navigation__items_item">Diary</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/CbmCalc" className="navigation__items_item navigation__items_item">CBM</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/invoice" className="navigation__items_item navigation__items_item">Invoice</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/about" className="navigation__items_item navigation__items_item">About</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/contact" className="navigation__items_item navigation__items_item">Contact</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/exim" className="navigation__items_item navigation__items_item">Exim</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/freight" className="navigation__items_item navigation__items_item">Freight</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/insurance" className="navigation__items_item navigation__items_item">Insurance</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/customClearance" className="navigation__items_item navigation__items_item">Custom</NavLink>
                 </div>
           </div>
       )
