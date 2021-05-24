ccac4a92e043451be80be3dd7de72d854752357f
anuj bhatt <anujbhatt023@gmail.com>
2021-05-24 13:50:17 +0530

    fixes

--- src/components/layout/fixed/navigationMob.jsx
+++ src/components/layout/fixed/navigationMob.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 import {NavLink} from "react-router-dom"
-
+import logo from "../../../assets/images/logo.png"
+import Backdrop from "../backdrop/backdrop"
 
   class NavigationMob extends Component{
 
@@ -16,16 +17,21 @@ import {NavLink} from "react-router-dom"
 
 
 
+
+
     render(){
 
       return (
           <div className="navMob">
 
                 <div className="navMob__toggler">
-                   {this.state.open?<i onClick={this.toggleHandler} className="fa fa-times" aria-hidden="true"></i>:<i onClick={this.toggleHandler} className="fa fa-bars" aria-hidden="true"></i>}
+                   {this.state.open?<i onClick={this.toggleHandler} className="fa fa-times" style={{color:"white",fontSize:"4rem"}} aria-hidden="true"></i>:<i onClick={this.toggleHandler} className="fa fa-bars" aria-hidden="true"></i>}
                 </div>
 
                 <div className={this.state.open?"openNav navMob__items ":"closeNav navMob__items "}>
+                    <div className="navigation__brandLogo">
+                         <NavLink  style={{border:"none"}} to="/"><img src={logo} alt=""/></NavLink>
+                    </div>
                     <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/" className="navigation__items_item navigation__items_item">Home</NavLink>
                     <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/diary" className="navigation__items_item navigation__items_item">Diary</NavLink>
                     <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/CbmCalc" className="navigation__items_item navigation__items_item">CBM</NavLink>
@@ -37,6 +43,7 @@ import {NavLink} from "react-router-dom"
                     <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/insurance" className="navigation__items_item navigation__items_item">Insurance</NavLink>
                     <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/customClearance" className="navigation__items_item navigation__items_item">Custom</NavLink>
                 </div>
+                {this.state.open?<Backdrop show={this.state.open} clicked={this.toggleHandler}/>:null}
           </div>
       )
     }
