c1bc81f9d2b05e0f9ff68f1ca258140aeefd2a72
anuj bhatt <anujbhatt023@gmail.com>
2021-06-06 21:27:42 +0530

    antim commit

--- src/components/layout/fixed/navigationMob.jsx
+++ src/components/layout/fixed/navigationMob.jsx
@@ -1,6 +1,8 @@
 import React, {Component} from "react"
 import {NavLink} from "react-router-dom"
 import logo from "../../../assets/images/logo.png"
+import greenCall from "../../../assets/images/greenCall.svg"
+import vBar from "../../../assets/images/vBar.svg"
 import Backdrop from "../backdrop/backdrop"
 
   class NavigationMob extends Component{
@@ -32,16 +34,17 @@ import Backdrop from "../backdrop/backdrop"
                     <div className="navigation__brandLogo">
                          <NavLink  style={{border:"none"}} to="/"><img src={logo} alt=""/></NavLink>
                     </div>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/" className="navigation__items_item navigation__items_item">Home</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/diary" className="navigation__items_item navigation__items_item">Diary</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/CbmCalc" className="navigation__items_item navigation__items_item">CBM</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/invoice" className="navigation__items_item navigation__items_item">Invoice</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/about" className="navigation__items_item navigation__items_item">About</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/contact" className="navigation__items_item navigation__items_item">Contact</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/exim" className="navigation__items_item navigation__items_item">Exim</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/freight" className="navigation__items_item navigation__items_item">Freight</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/insurance" className="navigation__items_item navigation__items_item">Insurance</NavLink>
-                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/customClearance" className="navigation__items_item navigation__items_item">Custom</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/" className="a navigation__items_item navigation__items_item">Home</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/diary" className="a navigation__items_item navigation__items_item">Diary</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/CbmCalc" className="a navigation__items_item navigation__items_item">CBM</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/invoice" className="a navigation__items_item navigation__items_item">Invoice</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/about" className="a navigation__items_item navigation__items_item">About</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/contact" className="a navigation__items_item navigation__items_item">Contact</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/exim" className="a navigation__items_item navigation__items_item">Exim</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/freight" className="a navigation__items_item navigation__items_item">Freight</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/insurance" className="a navigation__items_item navigation__items_item">Insurance</NavLink>
+                    <NavLink onClick={this.toggleHandler} activeStyle={{border: "3px solid #FFBF29", color:"white",background:"#FFBF29"}} exact to="/customClearance" className="a navigation__items_item navigation__items_item">Custom</NavLink>
+                      {this.state.open?<a style={{fontSize:"1.2rem",padding:"1rem",display:"flex",justifyContent:"center",alignItems:"center"}} href="tel://+918517885555" className=" navigation__items_item contact__1_text-call"><img style={{height:"1.5rem"}} className="contact__1_text-call--img1" src={greenCall} alt=""/><span>+91  8517885555</span></a>:null}
                 </div>
                 {this.state.open?<Backdrop show={this.state.open} clicked={this.toggleHandler}/>:null}
           </div>
